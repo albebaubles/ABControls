bd30ffb0ff05899074d4b29833da14eb6c781aeb
acorbett <al@stimwave.com>
2018-08-24 10:59:09 -0700

    minor bug fixes for tableviews

--- ABControls/ABListBox.swift
+++ ABControls/ABListBox.swift
@@ -166,11 +166,12 @@ import UIKit
         _tableview.alwaysBounceHorizontal = false
         _tableview.backgroundColor = UIColor.clear
         _tableview.frame = bounds
+            _tableview.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(_tableview)
         _tableview.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
         _tableview.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
         _tableview.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
         _tableview.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
-        addSubview(_tableview)
     }
     
     
